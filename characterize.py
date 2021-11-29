53a6ddb4700d814d64c44c10fd3c75104ace649c
Anuj Som <anuj.som@duke.edu>
2021-11-29 02:07:16 -0500

    Added testing_set_pipeline.py to help run analyses on the special test sets we recorded. Compiled confusion matrices into testing_confusion_matrices txt file

--- characterize.py
+++ characterize.py
@@ -32,23 +32,3 @@ if __name__ == "__main__":
     evaluate_model("fourier")
     evaluate_model("fourierdb")
     evaluate_model("voltage")
-
-    # filename = "trained_model.sav"
-    # loaded_model = pickle.load(open(filename, 'rb'))
-
-    # train_X, train_y, test_X, test_y = main("fourier")
-
-    # all_X = np.concatenate((test_X, train_X), axis=0)
-    # all_y = np.concatenate((test_y, train_y), axis=0)
-
-    # print("Model: {}".format(filename))
-    # print("Over all data: n={}".format(len(all_X)))
-    # result = loaded_model.score(all_X, all_y)
-    # print("Accuracy: {}".format(result))
-
-    # print("Over test data: n={}".format(len(test_X)))
-    # result = loaded_model.score(test_X, test_y)
-    # print("Accuracy: {}".format(result))
-
-    # c = sklearn.metrics.confusion_matrix(test_y, loaded_model.predict(test_X))
-    # print(c)
